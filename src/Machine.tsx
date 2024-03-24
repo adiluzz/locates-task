204d34797ef5208d0f0c9bcb87eeb3e2f3f8f2d4
adiluzz <adiluzz@gmail.com>
2024-03-24 13:28:06 +0200

    Added tables wrapper

--- src/Machine.tsx
+++ src/Machine.tsx
@@ -1,16 +1,17 @@
 import { FC } from "react";
 import MachineData from "./MachineData";
 import MachineTable from "./MachineTable";
+import { MachineTablesWrapper } from "./components";
 
 const Machine: FC<MachineData> = ({ name, allocations, suggested }) => {
     return <div>
         <h3>{name}</h3>
-        <div style={{ display: 'flex', flexDirection: 'row' }}>
+        <MachineTablesWrapper>
             <MachineTable allocations={allocations} />
             {
                 suggested && <MachineTable allocations={suggested} />
             }
-        </div>
+        </MachineTablesWrapper>
     </div>
 };
 
--- src/components.tsx
+++ src/components.tsx
@@ -4,4 +4,9 @@ export const TableWrapper = styled('div')`
     display: flex;
     flex-direction: column;
     justify-content: space-between;
-`
+`;
+
+export const MachineTablesWrapper = styled('div')`
+    display: flex;
+    flex-direction: row;
+`;
